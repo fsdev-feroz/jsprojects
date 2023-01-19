7a9eeb04a2279f0121a62e586c21f5aff985d0e2
fsdev-feroz <123072413+fsdev-feroz@users.noreply.github.com>
2023-01-19 16:42:58 +0530

    Add files via upload
    First program uploading to github

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("Hello World");
+let btn = document.getElementById("btn");
+btn.addEventListener("click",myFun)
+// console.log(btn);
+
+function myFun(){
+    let heading = document.querySelector("h1");
+    let inputValue = document.getElementById("inputValue").value;
+    if(inputValue != ''){ 
+    heading.innerHTML = inputValue;
+    document.querySelector("input").value = "";
+    }
+}
+// myFun()
